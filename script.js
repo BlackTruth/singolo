17705d1638779adca77769c6cd3da05f84e20019
Kanstantsin Kulahin <blacktruth1991@gmail.com>
2020-03-15 14:47:28 +0300

    feat: portfolio buttons sctipt

--- script.js
+++ script.js
@@ -1,5 +1,37 @@
-document.querySelector(".navbar").addEventListener("click", function(event) {
-    document.querySelectorAll(".navbar>span").forEach(elem => elem.classList.remove("active"));
-    event.target.classList.add("active");
-    location.href = event.target.querySelector("a").href;
+document.querySelector("#navbar").addEventListener("click", function(event) {
+  document
+    .querySelectorAll("#navbar>span")
+    .forEach(elem => elem.classList.remove("active"));
+  event.target.classList.add("active");
+  location.href = event.target.querySelector("a").href;
 });
+
+document
+  .querySelector("#portfolio-buttons")
+  .addEventListener("click", function(event) {
+    if (event.target.id == "portfolio-buttons") return;
+    let counter = 0;
+    let button;
+    let flag = false;
+
+    if (event.target.tagName == "SPAN") {
+      button = event.target.parentElement;
+    } else {
+      button = event.target;
+    }
+
+    document.querySelectorAll("#portfolio-buttons>div").forEach(elem => {
+      if (button != elem && !flag) {
+        counter++;
+      } else {
+        flag = true;
+      }
+      elem.classList.remove("active");
+    });
+    button.classList.add("active");
+    document
+      .querySelectorAll("#portfolioImages>.portfolio-image")
+      .forEach((elem, index) => {
+        elem.style.order = ( index + counter ) % 12;
+      });
+  });
