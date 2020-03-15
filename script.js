14cf0ee25f8e00834f33f1d03b1758dffffe95ea
Kanstantsin Kulahin <blacktruth1991@gmail.com>
2020-03-16 00:19:37 +0300

    feat: add alert message on submition form

--- script.js
+++ script.js
@@ -32,13 +32,50 @@ document
     document
       .querySelectorAll("#portfolioImages>.portfolio-image")
       .forEach((elem, index) => {
-        elem.style.order = ( index + counter ) % 12;
+        elem.style.order = (index + counter) % 12;
       });
   });
 
-  portfolioImages.addEventListener("click", function(event) {
-    portfolioImages
-      .querySelectorAll(".portfolio-image")
-      .forEach(elem => elem.classList.remove("bordered"));
-    event.target.classList.add("bordered");
+portfolioImages.addEventListener("click", function(event) {
+  portfolioImages
+    .querySelectorAll(".portfolio-image")
+    .forEach(elem => elem.classList.remove("bordered"));
+  event.target.classList.add("bordered");
+});
+
+function showAlert(subject, description) {
+  subject = subject.trim();
+  description = description.trim();
+  let fragment = document.createDocumentFragment();
+  let body = document.querySelector("body");
+  let wrapper = document.createElement("div");
+  wrapper.classList.add("alert-wrapper");
+  fragment.appendChild(wrapper);
+  let message = document.createElement("div");
+  message.classList.add("alert-message");
+  wrapper.appendChild(message);
+  let field = document.createElement("p");
+  field.innerText = "The letter was sent";
+  message.appendChild(field);
+  field = document.createElement("p");
+  if (subject) field.innerText = "Subject: " + subject;
+  else field.innerText = "Without subject";
+  message.appendChild(field);
+  field = document.createElement("p");
+  if (description) field.innerText = "Description: " + description;
+  else field.innerText = "Without description";
+  message.appendChild(field);
+  let button = document.createElement("input");
+  button.type = "button";
+  button.value = "Ok";
+  button.addEventListener("click", function() {
+    wrapper.remove();
   });
+  message.appendChild(button);
+  body.appendChild(fragment);
+}
+
+sendLetter.addEventListener("click", function(event) {
+  event.preventDefault();
+  showAlert(subject.value, description.value);
+});
